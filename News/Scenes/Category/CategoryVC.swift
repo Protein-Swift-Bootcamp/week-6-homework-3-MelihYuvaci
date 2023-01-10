8c395e1870bafdfdc5202e8dce8cf4ea5a1198be
MelihYuvaci <94677369+MelihYuvaci@users.noreply.github.com>
2023-01-10 16:14:56 +0300

    Add custom tableView Delegates & Datasources Methods and Add Navigation Controller

--- News/Scenes/Category/CategoryVC.swift
+++ News/Scenes/Category/CategoryVC.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class CategoryVC: UIViewController {
-
+  
+    @IBOutlet weak var tableView: UITableView!
+    
+    let categoriesArray = ["all","national","business","sports","world","politics","techology","politics","startup","entertainment","miscellaneous","hatke","science","automobile"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "CategoryCell", bundle: nil), forCellReuseIdentifier: "ReusableCell")
+        
     }
+    
+  
+    
 
+}
 
+//MARK: - TableViewDataSource
+
+extension CategoryVC :UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categoriesArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReusableCell",for: indexPath) as! CategoryCell
+        cell.label.text = categoriesArray[indexPath.row]
+        return cell
+    }
+    
+}
+
+//MARK: - TableViewDelegate
+
+extension CategoryVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "FeedVC") as? FeedVC{
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 }
 
