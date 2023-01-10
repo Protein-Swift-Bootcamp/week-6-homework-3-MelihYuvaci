614342a2bf3be0514e2b5f40c307da0655f9586d
MelihYuvaci <94677369+MelihYuvaci@users.noreply.github.com>
2023-01-10 14:01:29 +0300

    Upload Project Files

---
+++ News/Scenes/Category/CategoryVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Melih Yuvacı on 10.01.2023.
+//
+
+import UIKit
+
+class CategoryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
